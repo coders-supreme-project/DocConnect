2d1727aae2698f9dd375db379db34cdb8d34447d
Adem-Maafi <ademmaafi3@gmail.com>
2025-02-05 13:48:02 +0100

    search

---
+++ backend/Controller/user.controller.js
@@ -0,0 +1,17 @@
+const register = async (req, res) => {
+    // registration logic
+  };
+  
+  const login = async (req, res) => {
+    // login logic
+  };
+  
+  const session = async (req, res) => {
+    // session logic
+  };
+  
+  module.exports = {
+    register,
+    login,
+    session
+  };
